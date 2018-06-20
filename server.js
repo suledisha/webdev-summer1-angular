7844462bbcb61bd85159d76d9f0ba01cc34c8903
Disha <suledisha1@ccs.neu.edu>
2018-06-20 15:57:34 -0400

    Adding files to dist

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/sule-disha-angular-app'));
+app.use(express.static('./dist/webdev-summer1-angular'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/dist/sule-disha-angular-app/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/webdev-summer1-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
